7fae39f95a46943952a07d62d4471661ee33480e
aymanelyamoun <aymanelyamoun93@gmail.com>
2023-09-14 09:48:38 +0000

    solving db connection problem and starting the static website

--- srcs/requirements/wordpress/set_up.sh
+++ srcs/requirements/wordpress/set_up.sh
@@ -1,5 +1,8 @@
+wp core download
 wp config create --dbname=wordpress --dbuser=wordpress_user --dbhost=db --dbpass=pass --allow-root
-wp core install --url=localhost --title="a simple website" --admin_user=fine --admin_password=pass --admin_email=acoco112233@gmail.com --skip-email --allow-root
+wp core install --url='10.13.100.16' --title="a simple website" --admin_user=fine --admin_password=pass --admin_email=fakemail@gmail.com --skip-email --allow-root
+wp theme install zeever
+wp theme activate zeever
 wp user create uuuser bitchmail@gmail.com --user_pass=pass --allow-root
-
+/usr/sbin/php-fpm81 --nodaemonize
 
